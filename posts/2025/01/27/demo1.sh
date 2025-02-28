2e3b47f0fa3583eb959ca7b68ad2d5ab28e4cfd2
Karl Knechtel <zahlman@proton.me>
2025-02-28 18:54:40 -0500

    Track the scripts as separate files
    TODO: figure out a system to include the scripts from the Markdown.
    (Nikola has a "listings" feature, but it expects a template for
    generating separate listing pages, and it seems to only work in
    restructuredText mode.)

---
+++ posts/2025/01/27/demo1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright (c) 2025 Karl Knechtel.
+# Permission is granted to reproduce this code locally for testing purposes,
+# but please don't republish or redistribute it - instead, please direct
+# interested readers to this blog post at
+# https://zahlman.github.io/posts/2025/02/13/python-packaging-3/ .
+mkdir demo-0.1.0 # [1]
+cat << done_toml > demo-0.1.0/pyproject.toml # [2]
+[project]
+name = "demo"
+version = "0.1.0"
+dependencies = []
+[build-system]
+requires = [ ]
+build-backend = "build"
+backend-path = "."
+done_toml
+cat << done_info > demo-0.1.0/PKG-INFO # [3]
+Metadata-Version: 2.4
+Name: demo
+Version: 0.1.0
+done_info
+cat << done_setup > demo-0.1.0/build.py # [4]
+__import__('sys').exit("Arbitrary code could have been executed here.")
+done_setup
+tar czf demo-0.1.0.tar.gz demo-0.1.0/ # [5]
+pip download --no-deps --no-build-isolation ./demo-0.1.0.tar.gz # [6]
+rm -r demo-0.1.0/ demo-0.1.0.tar.gz
---
+++ posts/2025/01/27/demo2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) 2025 Karl Knechtel.
+# Permission is granted to reproduce this code locally for testing purposes,
+# but please don't republish or redistribute it - instead, please direct
+# interested readers to this blog post at
+# https://zahlman.github.io/posts/2025/02/13/python-packaging-3/ .
+mkdir demo_a-0.1.0 # [1]
+cat << done_toml > demo_a-0.1.0/pyproject.toml # [2]
+[project]
+name = "demo-b"
+version = "0.2.0"
+dependencies = [ ]
+description = ""
+[build-system]
+requires = [ "flit-core" ]
+build-backend = "flit_core.buildapi"
+done_toml
+cat << done_info > demo_a-0.1.0/PKG-INFO # [3]
+Metadata-Version: 2.4
+Name: demo-c
+Version: 0.3.0
+done_info
+touch demo_a-0.1.0/demo_b.py # [4]
+tar czf demo_d-0.4.0.tar.gz demo_a-0.1.0/ # [5]
+pip download ./demo_d-0.4.0.tar.gz # [6]
+rm -r demo_a-0.1.0/ demo_d-0.4.0.tar.gz
