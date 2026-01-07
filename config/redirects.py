76b5162aa44c6903164a476adf07243c20fa3d20
Karl Knechtel <zahlman@proton.me>
2026-01-06 23:00:02 -0500

    Add redirects for new URL scheme
    Canonical post URLs no longer contain Y/M/D path components.
    
    Added redirect specifications so that old bookmarks should work.
    
    In the future it may be nice to set up some kind of system that allows
    for optional Y/M/D components in URLs to just work (or with just Y or
    Y/M, even). But this isn't necessary for, e.g. driving the "posts by
    year" sidebar items, since these are based off post metadata rather than
    the folder structure per se.

--- config/redirects.py
+++ config/redirects.py
@@ -9,15 +9,33 @@ REDIRECTIONS = [
     # Nikola doesn't include categories in page URLs, or at least there
     # doesn't seem to be an obvious way to do that. (In the long run I
     # really want tags instead anyway). Also, we switched to pretty URLs.
-    # Redirect old URLs to the current scheme.
-    ("meta/2022/03/02/welcome.html", "/posts/2022/03/02/welcome"),
-    ("python-standard-library/2023/04/06/timing.html", "/posts/2023/04/06/timing"),
-    ("meta/2023/04/09/ah-yes-im-back-by-the-way.html", "/posts/2023/04/09/ah-yes-im-back-by-the-way"),
-    ("misc/2024/05/09/where-ive-been.html", "/posts/2024/05/09/where-ive-been"),
-    ("politics/the-psf/2024/07/31/an-open-letter-to-the-psf-coc-wg.html", "/posts/2024/07/31/an-open-letter-to-the-psf-coc-wg"),
-    ("politics/the-psf/2024/08/10/open-letter-psf-coc-wg-addendum-1-tim-peters.html", "/posts/2024/08/10/open-letter-psf-coc-wg-addendum-1-tim-peters"),
-    ("meta/2024/12/20/todo-finish-todo-list.html", "/posts/2024/12/20/todo-finish-todo-list"),
-    ("python-packaging/2024/12/24/python-packaging-1.html", "/posts/2024/12/24/python-packaging-1"),
-    ("meta/2024/12/31/todo-list-2.html", "/posts/2024/12/31/todo-list-2"),
-    ("posts/2025/01/07/stupid-pipx-tricks/index.html", "/posts/2025/01/07/python-packaging-2"),
+    # Also, canonical URLs now don't have date components (the system
+    # can still group posts by year, label the date in category posts etc.
+    # using explicit in-document metadata).
+    # Redirect old (and older) URLs to the current scheme.
+    ("meta/2022/03/02/welcome.html", "/posts/welcome"),
+    ("posts/2022/03/02/welcome/", "/posts/welcome"),
+    ("python-standard-library/2023/04/06/timing.html", "/posts/timing"),
+    ("posts/2023/04/06/timing/", "/posts/timing"),
+    ("meta/2023/04/09/ah-yes-im-back-by-the-way.html", "/posts/ah-yes-im-back-by-the-way"),
+    ("posts/2023/04/09/ah-yes-im-back-by-the-way/", "/posts/ah-yes-im-back-by-the-way"),
+    ("misc/2024/05/09/where-ive-been.html", "/posts/where-ive-been"),
+    ("posts/2024/05/09/where-ive-been/", "/posts/where-ive-been"),
+    ("politics/the-psf/2024/07/31/an-open-letter-to-the-psf-coc-wg.html", "/posts/an-open-letter-to-the-psf-coc-wg"),
+    ("posts/2024/07/31/an-open-letter-to-the-psf-coc-wg/", "/posts/an-open-letter-to-the-psf-coc-wg"),
+    ("politics/the-psf/2024/08/10/open-letter-psf-coc-wg-addendum-1-tim-peters.html", "/posts/open-letter-psf-coc-wg-addendum-1-tim-peters"),
+    ("posts/2024/08/10/open-letter-psf-coc-wg-addendum-1-tim-peters/", "/posts/open-letter-psf-coc-wg-addendum-1-tim-peters"),
+    ("meta/2024/12/20/todo-finish-todo-list.html", "/posts/todo-finish-todo-list"),
+    ("posts/2024/12/20/todo-finish-todo-list/", "/posts/todo-finish-todo-list"),
+    ("python-packaging/2024/12/24/python-packaging-1.html", "/posts/python-packaging-1"),
+    ("posts/2024/12/24/python-packaging-1/", "/posts/python-packaging-1"),
+    ("meta/2024/12/31/todo-list-2.html", "/posts/todo-list-2"),
+    ("posts/2024/12/31/todo-list-2/", "/posts/todo-list-2"),
+    ("posts/2025/01/01/new-year-new-blog/", "/posts/new-year-new-blog"),
+    ("posts/2025/01/07/stupid-pipx-tricks/", "/posts/python-packaging-2"),
+    ("posts/2025/01/07/python-packaging-2/", "/posts/python-packaging-2"),
+    ("posts/2025/01/11/a-brief-annotation/", "/posts/a-brief-annotation"),
+    ("posts/2025/01/24/leaning-in-to-my-ux/", "/posts/leaning-in-to-my-ux"),
+    ("posts/2025/02/28/python-packaging-3/", "/posts/python-packaging-3"),
+    ("posts/2025/12/31/oxidation/", "/posts/oxidation"),
 ]
